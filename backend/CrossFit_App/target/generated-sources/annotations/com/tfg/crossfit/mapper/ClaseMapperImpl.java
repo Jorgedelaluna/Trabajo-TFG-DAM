1f412711130c67bb4e45066afa50a0f014748c58
Roberto <robertomz95@gmail.com>
2026-04-01 00:12:30 +0200

    -- commit reserva2

--- backend/CrossFit_App/target/generated-sources/annotations/com/tfg/crossfit/mapper/ClaseMapperImpl.java
+++ backend/CrossFit_App/target/generated-sources/annotations/com/tfg/crossfit/mapper/ClaseMapperImpl.java
@@ -1,17 +1,37 @@
 package com.tfg.crossfit.mapper;
 
 import com.tfg.crossfit.dto.ClaseDTO;
+import com.tfg.crossfit.model.Actividad;
 import com.tfg.crossfit.model.Clase;
+import com.tfg.crossfit.model.Coach;
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-25T11:19:28+0100",
+    date = "2026-03-31T23:41:33+0200",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.44.0.v20251118-1623, environment: Java 21.0.8 (Eclipse Adoptium)"
 )
 @Component
 public class ClaseMapperImpl implements ClaseMapper {
+
+    @Override
+    public ClaseDTO toDTO(Clase entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        ClaseDTO claseDTO = new ClaseDTO();
+
+        claseDTO.setActividadNombre( entityActividadNombre( entity ) );
+        claseDTO.setCoachNombre( entityCoachNombre( entity ) );
+        claseDTO.setAforoMaximo( entity.getAforoMaximo() );
+        claseDTO.setDescripcion( entity.getDescripcion() );
+        claseDTO.setFechaHora( entity.getFechaHora() );
+        claseDTO.setId( entity.getId() );
+
+        return claseDTO;
+    }
 
     @Override
     public Clase toEntity(ClaseDTO dto) {
@@ -25,25 +45,37 @@ public class ClaseMapperImpl implements ClaseMapper {
         clase.setDescripcion( dto.getDescripcion() );
         clase.setFechaHora( dto.getFechaHora() );
         clase.setId( dto.getId() );
-        clase.setNombre( dto.getNombre() );
 
         return clase;
     }
 
-    @Override
-    public ClaseDTO toDTO(Clase entity) {
-        if ( entity == null ) {
+    private String entityActividadNombre(Clase clase) {
+        if ( clase == null ) {
             return null;
         }
+        Actividad actividad = clase.getActividad();
+        if ( actividad == null ) {
+            return null;
+        }
+        String nombre = actividad.getNombre();
+        if ( nombre == null ) {
+            return null;
+        }
+        return nombre;
+    }
 
-        ClaseDTO claseDTO = new ClaseDTO();
-
-        claseDTO.setAforoMaximo( entity.getAforoMaximo() );
-        claseDTO.setDescripcion( entity.getDescripcion() );
-        claseDTO.setFechaHora( entity.getFechaHora() );
-        claseDTO.setId( entity.getId() );
-        claseDTO.setNombre( entity.getNombre() );
-
-        return claseDTO;
+    private String entityCoachNombre(Clase clase) {
+        if ( clase == null ) {
+            return null;
+        }
+        Coach coach = clase.getCoach();
+        if ( coach == null ) {
+            return null;
+        }
+        String nombre = coach.getNombre();
+        if ( nombre == null ) {
+            return null;
+        }
+        return nombre;
     }
 }
--- backend/CrossFit_App/target/generated-sources/annotations/com/tfg/crossfit/mapper/UsuarioMapperImpl.java
+++ backend/CrossFit_App/target/generated-sources/annotations/com/tfg/crossfit/mapper/UsuarioMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-31T23:07:50+0200",
+    date = "2026-03-31T23:41:33+0200",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.44.0.v20251118-1623, environment: Java 21.0.8 (Eclipse Adoptium)"
 )
 @Component
@@ -22,8 +22,8 @@ public class UsuarioMapperImpl implements UsuarioMapper {
 
         Usuario usuario = new Usuario();
 
-        usuario.setNombre( dto.getNombre() );
         usuario.setEmail( dto.getEmail() );
+        usuario.setNombre( dto.getNombre() );
 
         return usuario;
     }
@@ -38,9 +38,9 @@ public class UsuarioMapperImpl implements UsuarioMapper {
 
         usuarioRespuestaDTO.setId( usuario.getId() );
         usuarioRespuestaDTO.setRol( usuario.getRol() );
-        usuarioRespuestaDTO.setNombre( usuario.getNombre() );
         usuarioRespuestaDTO.setEmail( usuario.getEmail() );
         usuarioRespuestaDTO.setEstadoCuota( usuario.getEstadoCuota() );
+        usuarioRespuestaDTO.setNombre( usuario.getNombre() );
 
         return usuarioRespuestaDTO;
     }
