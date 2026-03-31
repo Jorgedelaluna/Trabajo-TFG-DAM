45f631002d2d8ab05757b7bebdc1fdb38be49fd3
Roberto <robertomz95@gmail.com>
2026-04-01 01:37:57 +0200

    asdsd

--- frontend/src/pages/admin/usuarios/UsuarioListaPage.jsx
+++ frontend/src/pages/admin/usuarios/UsuarioListaPage.jsx
@@ -17,7 +17,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "../../../styles/Dashboard.css";
-import API_URL from "../api/api";
+import API_URL from "../../api/api";
 export default function UsuarioListaPage() {
 
   // Lista de usuarios obtenidos del backend
